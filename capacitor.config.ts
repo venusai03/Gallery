d092b3cb17a830f2e8e721723bfc2b251199676b
Venu Sai Madisetti <venu@betacrew.io>
2023-10-11 19:47:15 +0530

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'gallery',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+    appId: 'io.ionic.starter',
+    appName: 'gallery',
+    webDir: 'dist',
+    server: {
+        androidScheme: 'https'
+    }
 };
 
 export default config;
