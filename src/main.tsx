a6ce5c2e9f7c7f282e1a6b4339058ef523ee3266
Venu Sai Madisetti <venu@betacrew.io>
2023-10-11 21:34:07 +0530

    convert into pwa

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 defineCustomElements(window);
 const container = document.getElementById('root');
@@ -11,3 +12,5 @@ root.render(
         <App />
     </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
